dbbb3323e237112bc8d9bead641c96ef4ba1521e
Andy <byhook@163.com>
2018-10-29 13:29:40 +0800

    添加ffmpeg-hello模块

--- ffmpeg-merge/jni/Android.mk
+++ ffmpeg-merge/jni/Android.mk
@@ -6,7 +6,16 @@ LOCAL_MODULE := ffmpeg
 LOCAL_SRC_FILES := libffmpeg.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-# Program
+# hello-ffmpeg
+include $(CLEAR_VARS)
+LOCAL_MODULE := hello-ffmpeg
+LOCAL_SRC_FILES := hello_ffmpeg.c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_LDLIBS := -llog -lz
+LOCAL_SHARED_LIBRARIES := ffmpeg
+include $(BUILD_SHARED_LIBRARY)
+
+# now-streamer
 include $(CLEAR_VARS)
 LOCAL_MODULE := now-streamer
 LOCAL_SRC_FILES := now_streamer.c
